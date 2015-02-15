a9dc3256496c8b1b73c1b67c8cac6f71243219ba
nbeers22 <nbeers22@gmail.com>
2015-02-15 11:53:03 -0500

    Added devise views, root route, boostrap (css, js, and fonts)

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'home/index'
+
+  devise_for :users, controllers: {sessions: "users/sessions"}
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -7,7 +10,11 @@ Rails.application.routes.draw do
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
+  root to: 'home#index'
 
+  resources :users do
+
+  end
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
