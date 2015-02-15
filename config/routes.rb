96fed4cf369a21cb7222df2d7bfb9e30f13b2454
nbeers22 <nbeers22@gmail.com>
2015-02-15 13:33:05 -0500

    Updated devise fields to add columns, added simple_form gem, minor change to index view

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get 'home/index'
+  # get 'home/index'
 
-  devise_for :users, controllers: {sessions: "users/sessions"}
+  devise_for :users, controllers: {sessions: "users/sessions", registrations: "users/registrations"}
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
