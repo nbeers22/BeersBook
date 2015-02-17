6c91230fb5b2166242363b992f0ff3f7b6f56926
nbeers22 <nbeers22@gmail.com>
2015-02-17 09:58:11 -0500

    Added delete functionality with AJAX, styled a couple buttons

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,4 +2,12 @@ class HomeController < ApplicationController
   def index
   	@posts = Post.all
   end
+
+  def edit
+  	@post = Post.find(params[:id])
+  end
+
+  def destroy
+  
+  end
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,38 @@
 class PostsController < ApplicationController
+	respond_to :html, :js
+
 	def index
 
 	end
 
+	def create
+		@post = Post.create(post_params)
+	end
+
 	def new
-		
+		@post = Post.new
+	end
+
+	def edit
+		@post = Post.find(params[:id])
+	end
+
+	def update
+		@post.update_attributes(post_params)
+	end
+
+	def destroy
+		@post = Post.find(params[:id])
+		@post.destroy
+	    respond_to do |format|
+	      format.html
+	      format.js
+	    end
+	end
+
+	private
+
+	def post_params
+		params.require(:post).permit(:content)
 	end
 end
